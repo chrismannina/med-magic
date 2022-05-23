7a5c59b306ba0c1c22bd074afa59b698f6daa725
Chris Mannina <71852536+chrismannina@users.noreply.github.com>
2022-05-23 18:16:03 -0400

    Delete models.py

--- app/models.py
+++
@@ -1,9 +0,0 @@
-def validate_email(self, email):
-	user = Registration.query.filter_by(email = email.data).first()
-	if user is not None:
-		raise ValidationError('Please use a different email address')
-    
-def validate_username(self, username):
-	user = Registration.query.filter_by(username = username.data).first()
-	if user is not None:
-		raise ValidationError('Please use a different email address')
